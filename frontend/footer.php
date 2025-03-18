27497dca6666cc7afde7f7e59fe221736c91d99a
Felix-Zhonghan Lu <lu000214@algonquinlive.com>
2025-03-18 11:30:36 -0400

    php stuff

---
+++ frontend/footer.php
@@ -0,0 +1,6 @@
+<footer>
+    <ul>
+        <li>Privacy statements</li>
+        <li>Site map</li><!--Shows all the links to student library-->
+    </ul>
+</footer>
---
+++ frontend/header.php
@@ -0,0 +1,10 @@
+<header>
+    <div class ="navbar">
+        <a href="home.html">HOME</a>
+        <a href="Browse.html">BROWSE</a>
+        <a href="registration.html">REGISTRATION</a>
+        <a href="search.html">SEARCH</a>
+        <a href="myCollection.html">MY COLLECTION</a>
+        <a href="login.html">LOGIN</a>
+    </div>
+</header>
