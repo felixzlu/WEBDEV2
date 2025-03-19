3c29e54d3224fad319039d462f846427dac861e9
Felix-Zhonghan Lu <lu000214@algonquinlive.com>
2025-03-18 22:48:50 -0400

    css fixes

--- frontend/browse.php
+++ frontend/browse.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Browse</title>
-        <link rel ="stylesheet" href= "./Images_for_library/Browse.jpg">
+        <link rel ="stylesheet" href= "./css/style.css">
         <script src="./js/script.js"></script>
     </head>
     <body>
