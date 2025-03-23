d93b428e9963654269496b084bb4b98fd3722734
Felix-Zhonghan Lu <lu000214@algonquinlive.com>
2025-03-22 20:51:39 -0400

    typo

--- frontend/login.php
+++ frontend/login.php
@@ -14,7 +14,7 @@
     <!--Code for the login page-->
     <div class="login-container">
         <h2>Login</h2>
-        <p>Dont have an account?No problem, please <a href="./registration.html">Sign-UP</a></p>
+        <p>Dont have an account? No problem, please <a href="./registration.php">Sign-UP</a></p>
         <form action ="./login2.php" method ="POST">
             <div class ="input-group">
                 <label for ="username">Username: </label>
