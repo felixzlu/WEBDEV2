ed8a39b1721bcf7a039299da14188af761953461
Aravind Santha <aravindreddysanta@gmail.com>
2025-03-28 09:42:37 -0400

    aravind

--- frontend/js/scriptAddBook.js
+++ frontend/js/scriptAddBook.js
@@ -10,17 +10,24 @@ Due Date: March 30, 2025*/
 function validate(){
 
     let flag = true;
+/***Each field here monitored for the live updates from the user, so 
+ * the users can instantly see feedback as they type. If the input matches the
+ * error mesage disappers, if it doesnt, the error message appears and the border turns red.
+ */
+
+/***This code ensures the user enters correct title. */
 
     let regexTitle = /^(?!\s*$)[^']{1,255}$/;
     let errorTitle = document.getElementById("errortitle");
     let inputTitle = document.getElementById("title").value;
-    
+
     if (!regexTitle.test(inputTitle)) {
         errorTitle.innerHTML = "<p style='color: red;'>&#x274C Title should be non-empty, not have apostrophes, and not be longer than 255 characters.</p>";
         document.getElementById("title").style.borderColor = "red";
         flag = false;
     }
-    
+/***This code ensures the user enters the author */
+
     let regexAuthor = /^(?!\s*$)[^']{1,255}$/;
     let errorAuthor = document.getElementById("errorauthor");
     let inputAuthor = document.getElementById("author").value;
@@ -30,12 +37,12 @@ function validate(){
         document.getElementById("author").style.borderColor = "red";
         flag = false;
     }
-
+/***This code ensures the user enters the valid year, 4 digits */
     let regexYear = /^\d{4}$/;
     let errorYear = document.getElementById("erroryear");
     let inputYear = document.getElementById("year").value;
 
-    if (!regexYear.test(inputYear)) { // Validate against regex
+    if (!regexYear.test(inputYear)) { // Validate against putting incorrect year.
         errorYear.innerHTML = "<p style='color: red;'>&#x274C Year should be between 0000 and 9999 inclusive.</p>";
         document.getElementById("year").style.borderColor = "red";
         flag = false;
@@ -79,7 +86,7 @@ document.getElementById("title").addEventListener("input", function(){
         document.getElementById("title").style.borderColor = "red";
     }
 });
-
+/***This code is to validate the author using the regex */
 document.getElementById("author").addEventListener("input", function(){
 
     let regexAuthor = /^(?!\s*$)[^']{1,255}$/;
