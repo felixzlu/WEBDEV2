0773eadf38a4d96b83c5416c8e479adbcbf4c75b
Aravind Santha <aravindreddysanta@gmail.com>
2025-03-28 10:26:49 -0400

    aravind

--- frontend/addBook.php
+++ frontend/addBook.php
@@ -5,14 +5,16 @@
 <!--Student Names: Felix-Zhonghan Lu, Aravind Santha, Anas Eddaoualline-->
 <!--Due Date: March 30, 2025-->
 
-
+<!---This is the html to add a book to the database-->
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>addBooks</title>
+<!---External link to add the css stylesheet-->
         <link rel ="stylesheet" href="./css/style.css">
+<!---External link to add the javascript-->
         <script src="./js/scriptAddBook.js" defer></script>
     </head>
     <body>
@@ -21,27 +23,30 @@
         <form action="./addBook2.php" method="POST" onsubmit="return validate();">
             <fieldset><!--Helps in grouping all the elements together-->
                 <legend>ADD BOOK TO DATABASE</legend><!--Legend helps to create a title for the form-->
+
+                <!--Input field for the book title-->
                 <label for="title">Title: </label><!--This creates the names-->
                 <input type="text" id="title" name="title"><br>
                 <div id="errortitle"></div>
-
+               <!--Input field for the author title-->
                 <label for="author">Author: </label>
                 <input type="text" id ="author" name="author"><br>
                 <div id="errorauthor"></div>
-
+                <!--Input field for the year title-->
                 <label for="year">Year: </label>
                 <input type="text" id ="year" name="year"><br>
                 <div id="erroryear"></div>
-
+                <!--Input field for the Genre title-->
                 <label for="genre">Genre: </label>
                 <input type="text" id ="genre" name="genre"><br>
                 <div id="errorgenre"></div>
-
+                <!--Input field for the Desciption title-->
                 <label for="description">Description: </label>
                 <textarea rows="4" cols="50" id="description" name="description"></textarea>
                 <div id="errordesc"></div>
-
+                <!--submit button to send the form data-->
                 <input type="submit" value="Submit">
+                <!--Reset button to clear all input fields-->
                 <input type="reset">
 
             </fieldset>
--- frontend/addBook2.php
+++ frontend/addBook2.php
@@ -7,21 +7,24 @@
 
 
 <?php
-
+/***This includes the database credentails and database conenction functionalities*/
     require_once('../backend/db_credentials.php');
     require_once('../backend/database.php');
-    $db = db_connect();
-
+    $db = db_connect();//This establisht the database connection
+//This will check if the form was submitted via POST method.
     if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+       //Retrive form data from the POST request
         $title = $_POST['title'];
         $author = $_POST['author'];
         $year = $_POST['year'];
         $genre = $_POST['genre'];
         $description = $_POST['description'];
-
+/****This is the sql query to insert new books into the books table */
         $sql = "INSERT INTO books (title,author,year,genre,description) VALUES ('$title','$author','$year','$genre','$description')";
         $result = mysqli_query($db, $sql);
-        confirm_result_set($result);
+        confirm_result_set($result);//This will check if the query is successful
+        /**THis will redirect to the confirmation page with the book title as a URL parameter */
         header("Location: addBookConfirmation.php?title=$title");
     }
 
